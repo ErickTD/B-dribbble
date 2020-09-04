cdbe7fc95aa3459df0b54cf06437d085db05d9ff
Erick <T992187E@techdata.com>
2020-09-04 18:13:45 -0500

    feat: legal page init

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomeContainer from './home';
+import LegalContainer from './legal';
 
 const App = () => (
   <>
     <Router>
       <Switch>
+        <Route path="/legal" component={LegalContainer} />
         <Route path="/" component={HomeContainer} />
       </Switch>
     </Router>
---
+++ src/containers/legal/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../layout';
+
+const LegalContainer = () => (
+  <Layout>
+    <h1>Legal page</h1>
+  </Layout>
+);
+
+export default LegalContainer;

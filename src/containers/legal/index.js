478f2d34fa49c2900f61a07bc0ada1909f7278d5
Erick <T992187E@techdata.com>
2020-09-04 18:23:29 -0500

    feat: banner section

--- src/containers/legal/index.js
+++ src/containers/legal/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Layout from '../layout';
+import BannerContainer from './banner';
 
 const LegalContainer = () => (
   <Layout>
+    <BannerContainer />
     <h1>Legal page</h1>
   </Layout>
 );
